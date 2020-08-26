a01102a7405821f84a51ca5a449ba239b5ba2049
unknown <exgin3538@yahoo.com>
2020-08-26 08:56:54 -0400

    add resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import resume from '../assets/images/Reid_Samuel_Resume.pdf'
 
 const Footer = () => (
   <div id="footer">
@@ -7,13 +8,18 @@ const Footer = () => (
         <li>
           <a
             href="https://www.linkedin.com/in/sreid20/"
-            className="icon fa-linkedin"
+            className="icon fa-linkedin fa-2x"
           >
             <span className="label">LinkedIn</span>
           </a>
         </li>
         <li>
-          <a href="https://github.com/exgin" className="icon fa-github">
+          <a href="https://github.com/exgin" className="icon fa-github fa-2x">
+            <span className="label">Github</span>
+          </a>
+        </li>
+        <li>
+          <a href={resume} className="icon fa-file fa-2x">
             <span className="label">Github</span>
           </a>
         </li>
