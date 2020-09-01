268015beb8355e39542dd1a6367959a570f1c422
unknown <exgin3538@yahoo.com>
2020-09-01 16:27:01 -0400

    change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => (
         <img src={avatar} alt="" />
       </a> */}
       <h1>
-        I'm <strong>Samuel Reid</strong>, an aspiring software engineer
+        I'm <strong>Samuel Reid</strong>, a Software Engineer
       </h1>
     </div>
     <Footer />
